bbb6e126a222560830f815778f49211d42a61b6e
Javion <javionlog@163.com>
2020-09-28 10:55:56 +0800

    init

---
+++ src/components/Task.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default function Task(
+    { task: { id, title, state } },
+    onArchiveTask,
+    onPinTask
+) {
+    return (
+        <div className={`list-item ${state}`}>
+            <label className="checkbox">
+                <input
+                    type="checkbox"
+                    defaultChecked={state === 'TASK_ARCHIVED'}
+                    disabled={true}
+                    name="checked"
+                ></input>
+                <span
+                    className="checkbox-custom"
+                    onClick={() => onArchiveTask(id)}
+                ></span>
+            </label>
+            <div className="title">
+                <input
+                    type="text"
+                    value={title}
+                    readOnly={true}
+                    placeholder="Input title"
+                ></input>
+            </div>
+            <div
+                className="actions"
+                onClick={(event) => event.stopPropagation()}
+            >
+                {state !== 'TASK_ARCHIVED' && (
+                    // eslint-disable-next-line
+                    <a onClick={() => onPinTask(id)}>
+                        <span className={`icon-star`}></span>
+                    </a>
+                )}
+            </div>
+        </div>
+    )
+}
+
+Task.propTypes = {
+    task: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        state: PropTypes.string.isRequired,
+    }),
+    onArchiveTask: PropTypes.func,
+    onPinTask: PropTypes.func,
+}
---
+++ src/components/Task.stories.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { action } from '@storybook/addon-actions'
+
+import Task from './Task'
+
+export default {
+    component: Task,
+    title: 'Task',
+    excludeStories: /.*Data$/,
+}
+
+export const taskData = {
+    id: '1',
+    title: 'Test_Task',
+    state: 'TASK_INBOX',
+    updateAt: new Date(2020, 0, 1, 9, 0),
+}
+
+export const actionsData = {
+    onPinTask: action('onPinTask'),
+    onArchiveTask: action('onArchiveTask'),
+}
+
+export const Default = () => <Task task={{ ...taskData }} {...actionsData} />
+
+export const Pinned = () => (
+    <Task task={{ ...taskData, state: 'TASK_PINNED' }} {...actionsData} />
+)
+
+export const Archived = () => (
+    <Task task={{ ...taskData, state: 'TASK_ARCHIVED' }} {...actionsData} />
+)
